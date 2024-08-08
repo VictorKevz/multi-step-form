f9d34ec1bc3d55ddbcdc46bdcc23ffac023c7f20
Victor Kevz <58770966+VictorKuwandira@users.noreply.github.com>
2024-08-08 06:48:36 +0300

    Functionality step1-4 done

--- src/components/Step4/Step4.jsx
+++ src/components/Step4/Step4.jsx
@@ -30,13 +30,19 @@ function Step4({ formData, setFormData, decrementStep }) {
 
     // Calculate add-ons price
     if (checkbox1) {
-      total += isToggled ? addOnPrices.addOn1.yearly : addOnPrices.addOn1.monthly;
+      total += isToggled
+        ? addOnPrices.addOn1.yearly
+        : addOnPrices.addOn1.monthly;
     }
     if (checkbox2) {
-      total += isToggled ? addOnPrices.addOn2.yearly : addOnPrices.addOn2.monthly;
+      total += isToggled
+        ? addOnPrices.addOn2.yearly
+        : addOnPrices.addOn2.monthly;
     }
     if (checkbox3) {
-      total += isToggled ? addOnPrices.addOn3.yearly : addOnPrices.addOn3.monthly;
+      total += isToggled
+        ? addOnPrices.addOn3.yearly
+        : addOnPrices.addOn3.monthly;
     }
 
     return total;
@@ -54,21 +60,53 @@ function Step4({ formData, setFormData, decrementStep }) {
     <section className="step4 wrapper">
       <header className={`step4 step-header-container`}>
         <h2 className="step4 title">Review and Confirm</h2>
-        <p className="step4 parag">Review your selected options before confirming.</p>
+        <p className="step4 parag">
+          Review your selected options before confirming.
+        </p>
       </header>
-      <article className="step4-details-wrapper">
+      <article className="summary-wrapper">
         <div className="summary">
-          <h3>Subscription:</h3>
-          <p>{subscription} ({isToggled ? "Yearly" : "Monthly"})</p>
-          <h3>Add-ons:</h3>
-          {checkbox1 && <p>Add-on 1: {isToggled ? "+$50/yr" : "+$5/mo"}</p>}
-          {checkbox2 && <p>Add-on 2: {isToggled ? "+$100/yr" : "+$10/mo"}</p>}
-          {checkbox3 && <p>Add-on 3: {isToggled ? "+$150/yr" : "+$15/mo"}</p>}
-          <h3>Total:</h3>
-          <p>{isToggled ? `+$${total}/yr` : `+$${total}/mo`}</p>
+          <div className="sub-price-wrapper">
+            <div className="sub-change-wrapper">
+              <p>
+                {subscription} ({isToggled ? "Yearly" : "Monthly"})
+              </p>
+              <button className="change-btn">Change</button>
+
+            </div>
+            {subscription === "Arcade" && (
+              <p>{isToggled ? "$90/yr" : "$9/mo"}</p>
+            )}
+            {subscription === "Advanced" && (
+              <p>{isToggled ? "$120/yr" : "$12/mo"}</p>
+            )}
+            {subscription === "Pro" && (
+              <p>{isToggled ? "$150/yr" : "$15/mo"}</p>
+            )}
+          </div>
+          <div className="ads-on-wrapper">
+            <div className="service-option">
+              {checkbox1 && <p>Online Service</p>}
+              {checkbox2 && <p>Larger Storage</p>}
+              {checkbox3 && <p>Customizable Profile</p>}
+            </div>
+            <div className="service-price">
+              {checkbox1 && <p>{isToggled ? "+$10/yr" : "+$1/mo"}</p>}
+              {checkbox2 && <p>{isToggled ? "+$20/yr" : "+$2/mo"}</p>}
+              {checkbox3 && <p>{isToggled ? "+$20/yr" : "+$2/mo"}</p>}
+            </div>
+          </div>
+        </div>
+        <div className="total-wrapper">
+          <p className="total-cost">Total ({isToggled ? "per year" : "per month"})</p>
+          <h4>{isToggled ? `+$${total}/yr` : `+$${total}/mo`}</h4>
         </div>
         <div className="field button">
-          <button className="return step4" type="button" onClick={decrementStep}>
+          <button
+            className="return step4"
+            type="button"
+            onClick={decrementStep}
+          >
             Go Back
           </button>
           <button className="btn step4" type="button" onClick={handleSubmit}>
